530a838f4eb2e77f647ac9141ec15e72bfbe3813
Renan Verissimo de V. Souza <souza.sup.erp@alterdata.matriz>
2016-12-09 16:58:36 -0200

    Otimização calculo de ICMS + pequenos fixes.
    Ótimização em todos os tipos de cálculos de ICMS, para melhor manutenção e menos código.
    
    Ajuste nas funções de validação de campos.
    
    Pequenos ajustes na interface em geral.

--- CalculoICMS/controladorICMS.js
+++ CalculoICMS/controladorICMS.js
@@ -25,32 +25,96 @@ angular.module('calculoICMS')
         return IcmsProprio;
     })
 
-    .controller('controladorICMS', function($scope, $location, IcmsProprio){
+    .factory("IcmsBaseRed", function(){
+
+        var IcmsBaseRed = function(base, aliquota, aliquotaRed){
+            this.base = base;
+            this.aliquota = aliquota;
+            this.aliquotaRed = aliquotaRed;
+            this.valor = null;
+            this.valorDiscriminacao = null;
+            this._calculoRed();
+        }
+
+        IcmsBaseRed.prototype._calculoRed = function(){
+
+            if(!(this.base <= 0 || this.aliquota <= 0 || this.aliquota > 100 || this.aliquotaRed <= 0 || this.aliquotaRed > 100)){
+                
+                var resultado = ((this.base - (this.base*this.aliquotaRed/100))*this.aliquota/100);
+                this.valor = parseFloat(resultado).toFixed(3);
+                this.valorDiscriminacao = parseFloat(resultado).toFixed(2);   
+            }
+
+        }
+
+        return IcmsBaseRed;
+    })
+
+    .factory("IcmsDifal", function(){
+
+        var IcmsDifal = function(ano, base, aliquotaInterna, aliquotaInterestadual, aliquotaFcp){
+            this.ano = ano;
+            this.base = base;
+            this.aliquotaInterna = aliquotaInterna;
+            this.aliquotaInterestadual = aliquotaInterestadual;
+            this.aliquotaFcp = aliquotaFcp;
+            this.valorDifal = this.base*(this.aliquotaInterna - this.aliquotaInterestadual)/100;
+            this.valorFcp = this.base*this.aliquotaFcp/100;
+            this.valorOrigem = null;
+            this.valorDestino = null;
+            this.valorDestinoeFcp = null;
+            this._calculoDifal();
+        }
+
+        IcmsDifal.prototype._calculoDifal = function(){
+
+            if(!(this.ano <= 0 || this.base <= 0 || this.aliquotaInterna <= 0 || this.aliquotaInterna > 100 || this.aliquotaInterestadual <= 0 || this.aliquotaInterestadual > 100 || this.AliquotaInterna < this.aliquotaInterestadual || this.aliquotaFcp < 0 || this.aliquotaFcp > 100)){
+
+            var origem, destino;
+
+            if(this.ano <= 2016){
+
+                origem = this.valorDifal*0.60;
+                destino = (this.valorDifal*0.40) + this.valorFcp;
+            }
+
+            if(this.ano == 2017){
+
+                origem = this.valorDifal*0.40;
+                destino = (this.valorDifal*0.60) + this.valorFcp;
+            }
+
+            if(this.ano == 2018){
+
+                origem = this.valorDifal*0.20;
+                destino = (this.valorDifal*0.80) + this.valorFcp;
+            }
+
+            if(this.ano >= 2019){
+
+                origem = 0;
+                destino = this.valorDifal + this.valorFcp;
+            }
+
+                this.valorDifal= parseFloat(this.valorDifal).toFixed(2);
+                this.valorFcp = parseFloat(this.valorFcp).toFixed(2);
+                this.valorDestinoeFcp = parseFloat(destino).toFixed(2);
+                this.valorOrigem = parseFloat(origem).toFixed(3);
+                this.valorDestino = parseFloat(destino-this.valorFcp).toFixed(3);
+
+         }   
+    }
+
+        return IcmsDifal;
+    })
+
+    .controller('controladorICMS', function($scope, $location, IcmsProprio, IcmsBaseRed, IcmsDifal){
 
         $scope.valoresIcmsProprio = {}
 
-        $scope.IcmsBaseRed = {
+        $scope.valoresIcmsBaseRed = {}
 
-            'Base': $scope.baseIcmsRed,
-            'Aliquota': $scope.aliquotaIcms,
-            'AliquotaRed': $scope.aliquotaIcmsReduzida,
-            'Valor': $scope.valorIcmsRed,
-            'ValorDiscriminacao': $scope.valorIcmsRedDescriminicao,
-        }
-
-        $scope.IcmsDifal = {
-
-            'Ano': $scope.anoIcmsDifal,
-            'Base': $scope.baseIcmsDifal,
-            'AliquotaInterna': $scope.aliquotaIcmsInterna,
-            'AliquotaInterest': $scope.aliquotaIcmsInterest,
-            'AliquotaFcp': $scope.aliquotaFcp,
-            'Fcp': $scope.Fcp,
-            'ValorDifal': $scope.valorDifal,
-            'ValorOrigem': $scope.valorIcmsDifalOrigem,
-            'ValorDestino': $scope.valorIcmsDifalDestino,
-            'ValorDestinoFcp': $scope.valorDestinoFcp,
-        }
+        $scope.valoresIcmsDifal = {}
 
         $scope.validacaoBase = function(base){
 
@@ -63,9 +127,20 @@ angular.module('calculoICMS')
                 }
             }
 
-        $scope.validacaoAliquota = function(aliquota){
+        $scope.validacaoAliquotaNaoAceitaZero = function(aliquota){
             
              if(aliquota <= 0 || aliquota > 100){
+
+                    return false;
+                }else{
+
+                    return true;
+                }
+        }
+
+        $scope.validacaoAliquotaAceitaZero = function(aliquota){
+            
+             if(aliquota < 0 || aliquota > 100){
 
                     return false;
                 }else{
@@ -77,91 +152,19 @@ angular.module('calculoICMS')
         $scope.resultadoIcmsProprio = function(){
 
             var valor = new IcmsProprio($scope.valoresIcmsProprio.base, $scope.valoresIcmsProprio.aliquota);
-            $scope.valoresIcmsProprio = valor;
+            $scope.valoresIcmsProprio = valor;         
         }
 
-        $scope.icmsredbaseICMS =  function(){
+        $scope.resultadoIcmsRed =  function(){
 
-            if($scope.IcmsBaseRed.Base <= 0 || $scope.IcmsBaseRed.Aliquota <= 0 || $scope.IcmsBaseRed.AliquotaRed <= 0 || $scope.IcmsBaseRed.Aliquota > 100 || $scope.IcmsBaseRed.AliquotaRed > 100){
-
-                $scope.IcmsBaseRed.Valor = null; 
-                $scope.IcmsBaseRed.ValorDiscriminacao = null;
-            }else{
-
-                var resultado = ($scope.IcmsBaseRed.Base - ($scope.IcmsBaseRed.Base/100*$scope.IcmsBaseRed.Aliquota))*$scope.IcmsBaseRed.AliquotaRed/100;
-
-                $scope.IcmsBaseRed.Valor = parseFloat(resultado).toFixed(3);
-                $scope.IcmsBaseRed.ValorDiscriminacao = parseFloat(resultado).toFixed(2);
-            }
+            var valor = new IcmsBaseRed($scope.valoresIcmsBaseRed.base, $scope.valoresIcmsBaseRed.aliquota, $scope.valoresIcmsBaseRed.aliquotaRed);
+            $scope.valoresIcmsBaseRed = valor;
         }
 
-         $scope.icmsDifal = function(){
-
-            if($scope.IcmsDifal.Base == null){
-
-                alert("Valor da base de cálculo inválida! Verifique.");
-            }else{
-
-                if($scope.IcmsDifal.AliquotaInterna == null){
-
-                    alert("Valor da alíquota interna inválida! Verifique.");
-                }else{
-
-                    if($scope.IcmsDifal.AliquotaInterest == null){
-
-                        alert("Valor da alíquota interestadual inválida! Verifique.");
-                    }else{
-
-                        if($scope.IcmsDifal.AliquotaInterest > $scope.IcmsDifal.AliquotaInterna){
-
-                            alert("Valor da alíquota interestadual está maior que a interna! Verifique.");
-                        }else{
-
-                            if($scope.IcmsDifal.AliquotaFcp == null){
-                                
-                                alert("Valor da alíquota do FCP inválida! Verifique.");
-                            }else{
-
-                                $scope.IcmsDifal.ValorDifal = $scope.IcmsDifal.Base*($scope.IcmsDifal.AliquotaInterna - $scope.IcmsDifal.AliquotaInterest)/100;
-                                $scope.IcmsDifal.Fcp = ($scope.IcmsDifal.Base*$scope.IcmsDifal.AliquotaFcp)/100;
-
-                                var origem, destino;
-
-            if($scope.IcmsDifal.Ano <= 2016){
-
-                origem = $scope.IcmsDifal.ValorDifal*0.60;
-                destino = ($scope.IcmsDifal.ValorDifal*0.40) + $scope.IcmsDifal.Fcp;
-            }
-
-            if($scope.IcmsDifal.Ano == 2017){
-
-                origem = $scope.IcmsDifal.ValorDifal*0.40;
-                destino = ($scope.IcmsDifal.ValorDifal*0.60) + $scope.IcmsDifal.Fcp;
-            }
-
-            if($scope.IcmsDifal.Ano == 2018){
-
-                origem = $scope.IcmsDifal.ValorDifal*0.20;
-                destino = ($scope.IcmsDifal.ValorDifal*0.80) + $scope.IcmsDifal.Fcp;
-            }
-
-            if($scope.IcmsDifal.Ano >= 2019){
-
-                origem = 0;
-                destino = $scope.IcmsDifal.ValorDifal + $scope.IcmsDifal.Fcp;
-            }
-
-            $scope.IcmsDifal.ValorDifal = parseFloat($scope.IcmsDifal.ValorDifal).toFixed(2);
-            $scope.IcmsDifal.Fcp = parseFloat($scope.IcmsDifal.Fcp).toFixed(2);
-            $scope.IcmsDifal.ValorDestinoFcp = parseFloat(destino).toFixed(2);
-            $scope.IcmsDifal.ValorOrigem = parseFloat(origem).toFixed(3);
-            $scope.IcmsDifal.ValorDestino = parseFloat(destino-$scope.IcmsDifal.Fcp).toFixed(3);
-
-                            }
-                        }
-                    }
-                }        
-            }
+         $scope.resultadoIcmsDifal = function(){
+            var valor = new IcmsDifal($scope.valoresIcmsDifal.ano, $scope.valoresIcmsDifal.base, $scope.valoresIcmsDifal.aliquotaInterna, $scope.valoresIcmsDifal.aliquotaInterestadual, $scope.valoresIcmsDifal.aliquotaFcp);
+            $scope.valoresIcmsDifal = valor;
+            
         }
 
         $scope.limparProprio = function(form){
@@ -173,13 +176,13 @@ angular.module('calculoICMS')
         $scope.limparRed = function(form){
 
             form.$setUntouched();
-            $scope.IcmsBaseRed = null; 
+            $scope.valoresIcmsBaseRed = null; 
         }
 
         $scope.limparDifal = function(form){
 
             form.$setUntouched();
-            $scope.IcmsDifal = null;
+            $scope.valoresIcmsDifal = null;
         }
 
         $scope.voltar = function(){
